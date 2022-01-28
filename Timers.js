3fa96c5c25062d9e13f2c37925c981ed1ceb3043
syedafroze <safroze112@gmail.com>
2022-01-28 18:16:21 +0530

    updated

---
+++ Timers.js
@@ -0,0 +1,23 @@
+
+//setTimeout
+//setInterval
+//setImmediate 
+
+
+console.log("hello");
+
+// setTimeout(()=>{console.log("visit again");} , 2000);//
+
+//1000ms = 1sec
+
+
+
+
+let firstInterval = setInterval(()=>{console.log("hello");}, 3000);
+//for every 3sec it executes callbacl function
+
+//session id 
+setTimeout(()=>{clearInterval(firstInterval);} , 10000);
+
+console.log("Thank you");
+
---
+++ map.js
@@ -0,0 +1,55 @@
+let makeModel = {
+  price: "70k",
+};
+let makeModel2 = {
+  price: "70k",
+};
+
+let Obj = {
+  name: "dell",
+  [makeModel]: "80",
+  [makeModel2]: "90",
+};
+
+let map = new Map();
+
+map.set(makeModel, 80).set(makeModel2, 90); //timer 5 sec
+map.set("name", "Amit");
+
+console.log(map.get("name"));
+
+
+//-------------------------
+
+let loginCount = new Map(); //constructor method of Map
+
+let user1 = {
+  name: "syed",
+  id: 9,
+};
+
+let user2 = {
+  name: "Afroze",
+  id: 19,
+};
+
+let users = [
+  {
+    name: "syed",
+    id: 9,
+  },
+  {
+    name: "Afroze",
+    id: 19,
+  },
+  {
+    name: "Aditya",
+    id: 29,
+  },
+];
+
+for (let i = 0; i < users.length; i++) {
+  loginCount.set(users[i], i);
+}
+
+console.log(loginCount);
